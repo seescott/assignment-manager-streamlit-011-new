27037aff426cbaf31be8e97f906050d10488e562
Sofia <seescott@udel.edu>
2026-02-27 10:44:10 -0500

    New work done in class today

--- app_day2.py
+++ app_day2.py
@@ -4,6 +4,8 @@ st.title("Course Management App")
 st.header("Assignments")
 st.subheader("Assignment Manager")
 
+next_assignment_id_number = 3
+
 st.divider()
 
 assignments = [
@@ -37,6 +39,7 @@ due_date = st.date_input("Due Date")
 
 assignments_type = st.radio("Type",["homework", "lab"])
 
+points = st.number_input("Points")
 #assignments_type2 = st.selectbox("Type",["homework", "lab", "other"])
 
 #if assignments_type2 == "other":
@@ -47,9 +50,30 @@ assignments_type = st.radio("Type",["homework", "lab"])
 with st.expander("Assignment Preview",expanded=True):
     st.markdown("## Live Preview")
     st.markdown(f"Title: {title}")
+    st.markdown(f"Description: {description}")
+    st.markdown(f"Due Date: {due_date}")
+    st.markdown(f"Type: {assignments_type}")
 
-btn_save = st.button("Save", width="stretch")
+btn_save = st.button("Save", use_container_width=True, disabled=False)
+import time
 
 if btn_save:
-    st.warning("Working on it!")
+    with st.spinner("Saving Assignment..."):
+        time.sleep(5)
+        if title == "":
+            st.warning("Enter Assignment Title")
+        new_assignment_id = "HW_" + str(next_assignment_id_number)
+        next_assignment_id_number += 1
+        assignments.append({
+            "id" : new_assignment_id,
+            "title" : title,
+            "description" : description,
+            "points" : points,
+            "type" : assignments_type
+        }
+        )
+
+        st.success("Assignment is recorded!!")
+        st.dataframe(assignments)
+
 
